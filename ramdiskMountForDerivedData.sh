ab101fb4043695da77b47c6cd73ce40c3d283a95
Ilya Stroganov <ilya.stroganov@splytech.io>
2017-02-28 15:24:59 +0300

    force mode fix + code cleanup

--- ramdiskMountForDerivedData.sh
+++ ramdiskMountForDerivedData.sh
@@ -5,13 +5,9 @@
 ##
 
 _sizeMB=2048
-_blockCount=$(( ${_sizeMB} * 1024 * 1024 / 512 ))
-
 _mountPoint="${HOME}/Library/Developer/Xcode/DerivedData"
 _volumeName="DerivedData"
 
-_forceMode=
-_unmountMode=
 
 
 
@@ -19,6 +15,9 @@ _unmountMode=
 ############################################################
 ## parse options
 ##
+
+_forceMode=
+_unmountMode=
 
 while getopts "uf" opt; do
 	case "$opt" in
@@ -29,30 +28,126 @@ while getopts "uf" opt; do
 			_forceMode=1
 			;;
 		"?")
-			echo "Unknown option: $OPTARG"
+			echo "Unknown option: $opt"
 			echo
 			echo "Usage: $0 [-f] [-u]"
+			echo -e "\t -f 	force clean the ramdisk"
+			echo -e "\t -u	unmount ramdisk"
 			exit 1
 			;;
 	esac
 done
 
-
-
-
 ############################################################
 ## check if Xcode is running, kill if needed
 ##
 
-_procList="$( ps -ef | grep 'Contents/MacOS/Xcode' | grep -v grep )"
-	
-[[ -n "${_procList}" ]] && {
+[[ -n "$( ps -ceo comm | grep -E '\bXcode$' )" ]] && {
 	echo -n "Xcode is running --> terminating ... "
 	killall Xcode
 	sleep 2
 	echo "done"
 }
 
+############################################################
+## ramdisk routines
+##
+
+allocateAndFormatDisk() {
+	local _diskSizeMB="$1"
+	local _volumeName="$2"
+	local _blockCount=$(( ${_diskSizeMB} * 1024 * 1024 / 512 ))
+
+	## allocate new ram disk
+    echo -n "Allocating new RAM disk of size = [${_diskSizeMB}MB] ... " >&2
+    
+    local _diskName="$(echo $( hdid -nomount ram://${_blockCount} ))"
+    echo "done // disk = [${_diskName}]" >&2
+
+	formatDisk ${_diskName} ${_volumeName} >&2
+	
+	echo ${_diskName}
+	return
+}
+
+
+formatDisk() {
+	local _diskName="$1"
+	local _volumeName="$2"
+	
+	echo "Formatting [${_diskName}] // volume name = [${_volumeName}]" >&2
+	newfs_hfs -v ${_volumeName} ${_diskName} >&2	
+}
+
+
+mountDisk() {
+	local _diskName="$1"
+	local _mountPoint="$2"
+	
+	echo "Mounting [${_diskName}] at [${_mountPoint}]" >&2
+	mkdir -p ${_mountPoint} >&2
+	diskutil mount -mountPoint ${_mountPoint} ${_diskName} >&2
+}
+
+
+unmountDisk() {
+	local _diskName="$1"
+	
+	echo -n "Unmounting ... " >&2
+	echo "done // $( diskutil unmount ${_diskName} )" >&2
+	
+	echo -n "Ejecting ... " >&2
+	echo "done // $( diskutil eject ${_diskName} )" >&2
+}
+
+
+############################################################
+## check if disk is already mounted
+##
+
+_diskName="$( mount | grep " on ${_mountPoint} " | awk '{ print $1 }' )"
+
+[[ -n "${_diskName}" ]] && {
+	echo "RAM disk [${_diskName}] is found"
+	
+	## unmount mode :: unmount and eject
+    [[ -n "${_unmountMode}" ]] && {
+		unmountDisk ${_diskName}
+		exit
+	}
+	
+	## force clean disk
+    [[ -n "${_forceMode}" ]] && {
+		formatDisk ${_diskName} ${_volumeName}
+		exit
+	}
+	
+	## otherwise
+	echo " ==> use '-f' option to force cleanup"
+	echo
+	exit 1
+	
+} || {
+	## not mounted
+
+	[[ -n "${_unmountMode}" ]] && {
+		echo "RAM disk is not found"
+		echo
+		exit 1
+	}
+	
+	_diskName="$( allocateAndFormatDisk ${_sizeMB} ${_volumeName} )"
+	mountDisk ${_diskName} ${_mountPoint}
+}
+
+echo
+echo "done"
+echo
+
+
+
+
+exit
 
 
 
@@ -86,6 +181,8 @@ _diskName="$( mount | grep " on ${_mountPoint} " | awk '{ print $1 }' )"
 		echo
 		exit 1
 	}
+	
+	
 
 } || {
 	## not mounted
@@ -113,6 +210,7 @@ echo "Formatting [${_diskName}] // volume name = [${_volumeName}]"
 newfs_hfs -v ${_volumeName} ${_diskName}
 
 echo "Mounting [${_diskName}] at [${_mountPoint}]"
+mkdir -p ${_mountPoint}
 diskutil mount -mountPoint ${_mountPoint} ${_diskName}
 
 echo "Done"
