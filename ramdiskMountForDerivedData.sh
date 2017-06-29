5812cf1747519d0e9aa7885bc6c617bdbb49b827
Ilya Stroganov <ilya.stroganov@splytech.io>
2017-06-29 21:32:42 +0300

    added support of Xcode-beta.app restart + auto-kill processes which prevent the ramdisk from being unmounted

--- ramdiskMountForDerivedData.sh
+++ ramdiskMountForDerivedData.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env -S -P/usr/local/bin:/bin bash
 
 ############################################################
 ## settings
@@ -10,8 +10,6 @@ _volumeName="DerivedData"
 
 
 
-
-
 ############################################################
 ## parse options
 ##
@@ -19,7 +17,6 @@ _volumeName="DerivedData"
 _forceMode=
 _shouldUnmount=
 _shouldRemount=
-_shouldRestartXcode=
 
 while getopts "ufr" opt; do
 	case "$opt" in
@@ -49,12 +46,27 @@ done
 ## check if Xcode is running, kill if needed
 ##
 
-[[ -n "$( ps -ceo comm | grep -E '\bXcode$' )" && -z "${_forceMode}" ]] && {
-	echo -n "Xcode is running --> terminating ... "
-	killall Xcode
-	sleep 2
-	_shouldRestartXcode=1
-	echo "done"
+_appsToRestart=()
+_pidsToKill=()
+
+[[ -z "${_forceMode}" ]] && {
+	while read _line; do
+		_fields=( ${_line} )
+		
+		_pidsToKill+=("${_fields[0]}")
+		
+		_app="${_fields[1]%%.app*}"
+		_app="${_app##*/}"
+		_appsToRestart+=( ${_app} )
+
+	done < <(ps -eo pid,comm | grep -E '/Contents/MacOS/Xcode$')
+	
+	[[ -n "${_pidsToKill[@]}" ]] && {
+		echo -n "Xcode(s) running (${_appsToRestart[@]}) --> terminating ... "
+		kill -1 ${_pidsToKill[@]}
+		sleep 2
+		echo "done"
+	}	
 }
 
 ############################################################
@@ -78,6 +90,7 @@ allocateAndFormatDisk() {
 	return
 }
 
+############################################################
 
 formatDisk() {
 	local _diskName="$1"
@@ -87,6 +100,7 @@ formatDisk() {
 	newfs_hfs -J 32M -v "${_volumeName}" "${_diskName}" >&2	
 }
 
+############################################################
 
 mountDisk() {
 	local _diskName="$1"
@@ -98,12 +112,36 @@ mountDisk() {
 	mount -t hfs -o nodev -o noatime "${_diskName}" "${_mountPoint}"
 }
 
+############################################################
 
 unmountDisk() {
 	local _diskName="$1"
+	local _attempt="${2:-1}"
 	
-	echo -n "Unmounting ... " >&2
-	echo "done // $( diskutil unmount ${_diskName} )" >&2
+	[[ "${_attempt}" > 3 ]] && {
+		echo "Attempt limit exceeded" >&2
+		exit 1;
+	}
+
+	echo -n "Unmount attempt #${_attempt}... " >&2
+	
+	## unmount
+	_res="$( diskutil unmount "${_diskName}" 2>&1 )"
+	_status="$( [[ "$?" == "0" ]] && echo "done" || echo "ERROR" )";
+	echo "${_status} // ${_res}" >&2
+
+	## retry if needed
+	[[ "${_status}" == "ERROR" ]] && {
+		for _pid in "$( echo "${_res}" | perl -ne 's/PID=(\d+)/ print "$1\n"; /ge' )"; do
+			echo "--> killing PID=${_pid}" >&2
+			kill "${_pid}";
+		done
+
+		sleep 1
+		unmountDisk "${_diskName}" "$(( ${_attempt} + 1 ))"
+		return
+	}
+
 	
 	echo -n "Ejecting ... " >&2
 	echo "done // $( diskutil eject ${_diskName} )" >&2
@@ -128,10 +166,11 @@ _diskName="$( mount | grep " on ${_mountPoint} " | awk '{ print $1 }' )"
 			mountDisk ${_diskName} ${_mountPoint}
 		}
 
-		## restart Xcode
-		[[ -n "${_shouldRestartXcode}" ]] && {
-			open -a xcode
-		}
+		## restart Xcode(s)
+		for _app in ${_appsToRestart[@]}; do
+			echo "Restarting ${_app}"
+			open -a ${_app}
+		done
 
 		exit
 	}
@@ -162,6 +201,8 @@ _diskName="$( mount | grep " on ${_mountPoint} " | awk '{ print $1 }' )"
 	_diskName="$( allocateAndFormatDisk ${_sizeMB} ${_volumeName} )"
 	mountDisk ${_diskName} ${_mountPoint}
 }
+
+############################################################
 
 echo
 echo "done"
